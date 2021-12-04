e49baae4786f3be0d187b3cc2f38c95ca7d1e8e9
xfrnk2 <xfrnk2@gmail.com>
2021-12-04 19:57:55 +0900

    refactor: 역 이름 등록, 삭제 이전 유효성 검사를 수행하도록 변경

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -8,19 +8,21 @@ import java.util.Objects;
 import subway.util.InputValidator;
 
 public class StationRepository {
-    private static final List<Station> stations = new ArrayList<>();
+	private static final List<Station> stations = new ArrayList<>();
 
-    public static List<Station> stations() {
-        return Collections.unmodifiableList(stations);
-    }
+	public static List<Station> stations() {
+		return Collections.unmodifiableList(stations);
+	}
 
-    public static void addStation(String stationName) {
-		InputValidator.checkValidName(stationName);
-        stations.add(new Station(stationName));
-    }
+	public static void addStation(String stationName) {
+		if (InputValidator.checkValidName(stationName) && !InputValidator.isDuplicatedStationName(stationName)) {
+			stations.add(new Station(stationName));
+		}
+	}
 
-    public static void deleteStation(String stationName) {
-        InputValidator.checkValidName(stationName);
-        stations.removeIf(station -> Objects.equals(station.getName(), stationName));
-    }
+	public static void deleteStation(String stationName) {
+		if (InputValidator.checkValidName(stationName)) {
+			stations.removeIf(station -> Objects.equals(station.getName(), stationName));
+		}
+	}
 }
--- src/main/java/subway/util/InputValidator.java
+++ src/main/java/subway/util/InputValidator.java
@@ -9,13 +9,16 @@ import subway.view.OutputView;
 
 public class InputValidator {
 
-	public static void checkValidName(final String name) {
+	public static boolean checkValidName(final String name) {
 
 		if (name == null || name.isEmpty()) {
 			OutputView.printError("빈 값은 입력할 수 없습니다.");
+			return false;
 		} else if (name.length() < 2){
 			OutputView. printError("2글자 미만 이름을 입력하셨습니다.");
+			return false;
 		}
+		return true;
 	}
 
 	public static boolean isDuplicatedStationName(String stationName) {
